48e962410af2b35d1b901c60d16738ea261b7394
Jeremy Kuhn <jeremy.kuhn@winterframework.io>
2021-05-17 12:09:38 +0200

    Add release.sh

---
+++ release.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e
+
+if (( $# == 0 || $# > 2 ))
+	then
+		echo "Usage: release.sh <RELEASE_VERSION> [<NEW_SNAPSHOT_VERSION>]"
+		exit 0
+elif ! [[ "$1" =~ ^([0-9]+)$ ]]
+	then
+		echo "Invalid release version: $1, expected x"
+		exit 1
+elif (( $# == 2 )) && ! [[ "$2" =~ ^([0-9]+)-SNAPSHOT$ ]] 
+	then
+		echo "Invalid snapshot version: $2, expected x-SNAPSHOT"
+		exit 1
+fi
+
+if (( $# > 0 ))
+	then
+		echo "===== Releasing: $1 ====="
+		mvn versions:set -DgenerateBackupPoms=false -DnewVersion=$1
+		git commit -a -m "Release $1"
+		git tag -a $1 -m "Release $1"
+		mvn clean deploy -Pio.winterframework.release
+fi
+
+if (( $# == 2 ))
+	then
+		if ! [[ "$2" =~ ^([0-9]+)\.([0-9]+)\.([0-9]+)-SNAPSHOT$ ]] 
+			then
+				echo "bad version"
+		fi
+		echo "===== New Snapshot: $2 ====="
+		mvn versions:set -DgenerateBackupPoms=false -DnewVersion=$2
+		git commit -a -m "$2"
+fi
+
+exit 0
